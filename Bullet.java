ff74db6ac79a146d1e63f06278eebe83ab4ade60
Sagar <57942799+poudel-sagar@users.noreply.github.com>
2020-05-21 23:42:29 +0545

    Create Bullet.java

---
+++ Bullet.java
@@ -0,0 +1,30 @@
+package com.example.last.bird_game;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import static com.example.last.bird_game.GameView.screenRatioX;
+import static com.example.last.bird_game.GameView.screenRatioY;
+
+public class Bullet {
+    int x,y,width,height;
+    Bitmap bullet;
+    Bullet (Resources res)
+    {
+        bullet = BitmapFactory.decodeResource(res,R.drawable.bullet);
+        width=bullet.getWidth();
+        height=bullet.getWidth();
+
+        width/=4;
+        height/=4;
+        width*=(int) screenRatioX;
+        height*=(int) screenRatioY;
+        bullet=Bitmap.createScaledBitmap(bullet,width,height,false);
+    }
+    Rect getCollisionShape(){
+        return new Rect(x,y,x+width,y +height);
+    }
+
+}
